c9f954a1a6dc5931eb66161f27046e9ce0cb8efe
estevo <pedro.cordova2002@gmail.com>
2023-09-20 19:28:03 -0300

    visitor pattern done

--- untitled/src/developer/visitorPattern/CreditCard.java
+++ untitled/src/developer/visitorPattern/CreditCard.java
@@ -2,54 +2,29 @@ package developer.visitorPattern;
 
 public abstract class CreditCard {
 
-    public abstract void doSomething();
-    public abstract void doSomething1();
-    public abstract void doSomething2();
-    public abstract void doSomething3();
+    public abstract <T> T doSomething(CardVisitor<T> cardVisitor);
+}
+
+interface CardVisitor<T> {
+
+    public T visit(Visa visa);
+
+    public T visit(MasterCard masterCard);
+
 }
 
 class Visa extends CreditCard {
 
     @Override
-    public void doSomething() {
-
-    }
-
-    @Override
-    public void doSomething1() {
-
-    }
-
-    @Override
-    public void doSomething2() {
-
-    }
-
-    @Override
-    public void doSomething3() {
-
+    public <T> T doSomething(CardVisitor<T> cardVisitor) {
+        return cardVisitor.visit(this);
     }
 }
 
 class MasterCard extends CreditCard {
 
     @Override
-    public void doSomething() {
-
-    }
-
-    @Override
-    public void doSomething1() {
-
-    }
-
-    @Override
-    public void doSomething2() {
-
-    }
-
-    @Override
-    public void doSomething3() {
-
+    public <T> T doSomething(CardVisitor<T> cardVisitor) {
+        return cardVisitor.visit(this);
     }
 }
