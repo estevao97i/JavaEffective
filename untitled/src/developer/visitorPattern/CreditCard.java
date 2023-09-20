ceb6d08b68c8736d81c2573969de27688cf8f33c
estevo <pedro.cordova2002@gmail.com>
2023-09-20 19:17:45 -0300

    begin of visitor Pattern

--- untitled/src/developer/visitorPattern/CreditCard.java
+++ untitled/src/developer/visitorPattern/CreditCard.java
@@ -1,4 +1,55 @@
 package developer.visitorPattern;
 
-public class CreditCard {
+public abstract class CreditCard {
+
+    public abstract void doSomething();
+    public abstract void doSomething1();
+    public abstract void doSomething2();
+    public abstract void doSomething3();
+}
+
+class Visa extends CreditCard {
+
+    @Override
+    public void doSomething() {
+
+    }
+
+    @Override
+    public void doSomething1() {
+
+    }
+
+    @Override
+    public void doSomething2() {
+
+    }
+
+    @Override
+    public void doSomething3() {
+
+    }
+}
+
+class MasterCard extends CreditCard {
+
+    @Override
+    public void doSomething() {
+
+    }
+
+    @Override
+    public void doSomething1() {
+
+    }
+
+    @Override
+    public void doSomething2() {
+
+    }
+
+    @Override
+    public void doSomething3() {
+
+    }
 }
